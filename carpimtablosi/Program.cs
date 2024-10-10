3a39fd75f6c7aa0d2eec75fbd73b80c5463fe6ca
batuhanyilmaz2814 <batuhanyilmaz2814@outlook.com>
2024-10-10 21:27:13 +0300

    Add project files.

---
+++ carpimtablosi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carpimtablosi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+            for (int i = 1; true ; i++)
+            {
+                for (j = 0; j <= 10; j++)
+                {
+                    Console.WriteLine(i + " * " + j + " = " + (i * j));
+                    System.Threading.Thread.Sleep(700);
+
+
+
+
+                }
+                j = 0;
+
+
+
+
+            }
+        }
+    }
+}
